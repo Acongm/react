905739c652a30e75a86a80425aef749fb699e302
acongm <acongm@126.com>
2022-07-25 00:02:26 +0800

    feat: init project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-README.md
+react 学习项目
---
+++ react16/src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+const container = document.getElementById("app");
+ReactDOM.render(<App />, container);
